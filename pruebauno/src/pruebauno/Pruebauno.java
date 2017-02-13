e6a1793e12e24d29ffaa225f3d10af471192f514
Alumno <Alumno@DESKTOP-9JBMKJQ.itver.edu.mx>
2017-02-13 15:55:33 -0600

    proyectobase

---
+++ pruebauno/src/pruebauno/Pruebauno.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebauno;
+import javax.
+
+/**
+ *
+ * @author Alumno
+ */
+public class Pruebauno {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame ventana1 = New JFrame("ventana de prueba");
+        ventana1.setSize(320,200);
+        Venatana1.setVisible(true);
+    }
+    
+}
